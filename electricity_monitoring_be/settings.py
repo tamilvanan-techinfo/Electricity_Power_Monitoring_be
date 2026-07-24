ca0a6b6b59031dfe94fad1327bc3f5afead00aa6
tamil <tamil@gmail.com>
2026-07-24 12:31:03 +0530

    local server db path

--- electricity_monitoring_be/settings.py
+++ electricity_monitoring_be/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-*!!lvjt-79w0e$b^o6ixcdea@ioc6syhy#t45)whqboi&esiez
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -64,12 +64,14 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://172.25.32.1:8000/"
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'electricity_monitoring_be.urls'
 CSRF_TRUSTED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:5173",
+    "http://172.25.32.1:8000/"
 ]
 TEMPLATES = [
     {
